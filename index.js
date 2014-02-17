9be5dd9a5c97b25fe8596eb2d369c55390e7f3af
Antoine Lehurt <keuwah@gmail.com>
2014-02-17 15:56:16 +0100

    Fix SCSS render
    - We need to be able to compile files that are not in the root.
    - Added globalLookupPaths to `loadPaths` instead of local component dep dir (/component).
    - Don't need to read the file, we can specify the file path to node-sass.

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var sass = require('node-sass');
 var path = require('path');
-var fs = require('fs');
 var async = require('async');
 
 module.exports = function(builder) {
   builder.hook('before styles', function(pkg, next) {
-
     // No styles in this package
-    if(pkg.root !== true || !pkg.config.styles) return next();
+    if (!pkg.config.styles) return next();
 
     // Get all the scss files from the scripts list
     var sassfiles = pkg.config.styles.filter(function(file) {
@@ -18,38 +16,21 @@ module.exports = function(builder) {
     if (sassfiles.length === 0) return next();
 
     // Sass load paths
-    var loadPaths = (pkg.config.paths || []).map(pkg.path, pkg).concat(pkg.path('components'));
+    var loadPaths = (pkg.config.paths || [])
+      .map(pkg.path, pkg)
+      .concat(pkg.globalLookupPaths);
 
-    // Get the real path for each file relative to the package
-    // Must pass in package as context to make sure we have the correct context
-    var realSassFiles = sassfiles.map(pkg.path, pkg);
-
-    // Function to compile sass files that will include the package
-    // load paths as Sass load paths
-    var compileSassFile = function(str, callback) {
-       sass.render(str, callback, { includePaths: loadPaths });
-    };
-
-    // Read all the sass files
-    async.map(sassfiles, fs.readFile, function(err, results){
-      if(err) return next(err);
-
-      results = results.map(function(data, i){
-        return data.toString();
+    // Compile them all
+    async.each(sassfiles, function(file, cb) {
+      var css = sass.renderSync({
+        file: pkg.path(file),
+        includePaths: loadPaths
       });
 
-      // Compile them all
-      async.map(results, compileSassFile, function(err, compiledFiles){
-        if(err) return next(err);
+      pkg.removeFile('styles', file);
+      pkg.addFile('styles', path.basename(file, path.extname(file)) + '.css', css);
 
-        compiledFiles.forEach(function(data, i){
-          pkg.addFile('styles', sassfiles[i], data);
-          pkg.removeFile('styles', sassfiles[i]);
-        });
-
-        next();
-      });
-    });
-
+      cb();
+    }, next);
   });
 };
